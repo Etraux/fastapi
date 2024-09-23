0f08a39737c7fb258f9a609ecd90777e5ee3cee7
Alex Z <azhdan073@gmail.com>
2024-09-23 21:09:21 +0300

    some basic i/o for testing postman

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
+
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return{"message":"Hello World"}
+    return{"message":"Welcome!"}
+
+@app.get("/posts")
+async def get_posts():
+    return {"data":"This is your post"}
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post":f"title: {payload['title']} content: {payload['content']}"}
